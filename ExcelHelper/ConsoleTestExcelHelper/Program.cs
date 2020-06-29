5b38fb4bf82a8ef671e4c3a3597679bd38279b17
toddhyatt <todd.hyatt@live.com>
2020-06-29 03:44:57 -0500

    ProjectSetup

---
+++ ExcelHelper/ConsoleTestExcelHelper/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTestExcelHelper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(ExcelHelper.ExcelApp app = new ExcelHelper.ExcelApp(@"c:\temp\test.txt"))
+            {
+                Console.WriteLine(app.DoesFileExist().ToString()); ;
+                
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ExcelHelper/ExcelHelper/ExcelApp.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Dynamic;
+using System.IO;
+
+namespace ExcelHelper
+{
+    public class ExcelApp : IDisposable
+    {
+        private bool disposedValue;
+        public string FileName { protected set; get; }
+
+        public ExcelApp(string sFileName )
+        {
+            FileName = sFileName;
+        }
+
+        public bool DoesFileExist()
+        {
+            return System.IO.File.Exists(FileName);
+        }
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects)
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+                // TODO: set large fields to null
+                disposedValue = true;
+            }
+        }
+
+        // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+        // ~ExcelHelper()
+        // {
+        //     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        //     Dispose(disposing: false);
+        // }
+
+        void IDisposable.Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
